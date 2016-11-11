87291feccbca3b78181d8df0f6bbd289d4f355e6
Tanmay Singh <tanmaysingh1226@gmail.com>
2016-11-12 02:54:34 +0530

    initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed;
+	private int count ;
+	public Text countText;
+	public Text winText;
+	public Text play;
+	public Button playButton;
+	public Text TGZball;
+	private Light myLight;
+	public GameObject wallObject;
+	private bool paused = false;
+
+	// Use this for initialization
+	void Start () {
+		count = 0;
+		rb = GetComponent<Rigidbody> ();
+		myLight = GetComponent<Light> ();
+	
+		setCountText ();
+		winText.text = "";
+		paused = true;
+		play.text = "P L A Y";
+		playButton.onClick.AddListener (() => {
+			OnResumeGame();
+			playButton.gameObject.SetActive(false); 
+			TGZball.fontSize = 14;
+			TGZball.gameObject.transform.Translate(new Vector3(0,50,0));
+			
+		}); 
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnPauseGame ()
+	{
+		paused = true;
+		play.text =  "R E S U M E";
+	}
+
+
+	void OnResumeGame ()
+	{
+		paused = false;
+
+	}
+
+	void FixedUpdate(){
+		
+		if(!paused){
+			
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement*speed);
+
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+			count += 100;
+			setCountText ();
+		}
+	}
+
+	void setCountText(){
+		countText.text = "Count : " + count.ToString ();
+		if (count >= 1200) {
+			winText.text = "Y  O  U    W  I  N";
+		//	wallObject.gameObject.SetActive (false);
+		}
+
+
+	}
+}
+ 
---
+++ Assets/Scripts/prefabScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class prefabScript : MonoBehaviour {
+
+	private Light myLight;
+	private float phaseDiff;
+	private GameObject player;
+	public Vector3 oldPosition;
+	public float freqMult= 3.0f;
+	public float minDist = 11.0f;
+	private float oldTime;
+	private float oldValue;
+
+
+	// Use this for initialization
+	void Start () {
+		myLight = this.GetComponent<Light> ();
+		phaseDiff = UnityEngine.Random.value * Mathf.PI;
+		player = GameObject.FindGameObjectWithTag ("Player");
+	   
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		oldPosition = this.transform.position-player.transform.position;
+
+		float t = Time.time;
+		float w = valueGive (oldPosition);
+		float p = oldTime * oldValue  - oldTime * w;
+		float m = Mathf.Sin (t*w + p);
+
+
+		myLight.intensity = 4.0f * m * m + 3.0f;
+		oldTime = t;
+		oldValue = w;
+
+	}
+	float valueGive(Vector3 pos){
+		float endval = 1 / (1 + UnityEngine.Mathf.Exp (pos.magnitude/minDist));	
+		return endval*freqMult+0.5f;
+	}
+}
+
